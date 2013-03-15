c60d7e6ae186cf4305014ac14dec4194e9ee94b0
codebot <devnull@localhost>
2013-03-15 13:15:10 -0700

    blinking led works on psu

--- firmware/psu/main.c
+++ firmware/psu/main.c
@@ -3,11 +3,18 @@
 #include <avr/wdt.h>
 #include <util/delay.h>
 
+// hardware connections:
+//  PC3 = ssr anode
+//  PC2 = red LED
+
 int main()
 {
   wdt_disable(); // todo: revisit this...
+  DDRC = 0x0c;
   while (1)
   {
+    _delay_ms(250);
+    PORTC ^= 0x04;
   }
   return 0;
 }

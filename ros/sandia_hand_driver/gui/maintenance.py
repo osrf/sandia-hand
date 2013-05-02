77cd03d129c21e2dda911fcf949dad3b6a7316be
Morgan Quigley <morgan@osrfoundation.org>
2013-05-02 00:50:31 -0700

    factor firmware install and test GUI so there isn't any duplicated code between f2 and f3 GUIs

--- ros/sandia_hand_driver/gui/maintenance.py
+++ ros/sandia_hand_driver/gui/maintenance.py
@@ -7,59 +7,152 @@ import roslib; roslib.load_manifest('sandia_hand_driver')
 import rospy, math, signal
 from sandia_hand_msgs.msg import RawFingerState
 
+class TactileBoardTab(QWidget):
+  def __init__(self, board_name, num_taxels, exit_on_success):
+    super(TactileBoardTab, self).__init__()
+    self.board_name = board_name
+    self.num_taxels = num_taxels
+    self.exit_on_success = exit_on_success
+    self.shd = roslib.packages.get_pkg_dir('sandia_hand_driver')
+    self.driver_process = None
+    self.bootloader_btn = QPushButton("Install Bootloader")
+    self.bootloader_btn.clicked.connect(self.bootloaderClicked)
+    self.application_btn = QPushButton("Install Application")
+    self.application_btn.clicked.connect(self.applicationClicked)
+    self.auto_btn = QPushButton("Awesome")
+    self.auto_btn.clicked.connect(self.autoClicked)
+    self.terminal = QTextEdit()
+    self.terminal.setStyleSheet("QTextEdit { background-color: black; color: white;}")
+    self.test_grid = QGridLayout()
+    self.test_grid.setSpacing(5)
+    self.test_grid.setColumnMinimumWidth(0, 15)
+    self.test_grid.setColumnStretch(2, 1)
+    self.test_grid.addWidget(QLabel("Accelerometer magnitude"), 0, 2)
+    self.accel_mag_light = QLabel() #"FAIL")
+    self.accel_mag_light.setStyleSheet("QWidget {background-color:red}")
+    self.accel_mag_label = QLabel("0")
+    self.tactile_min = [65535] * self.num_taxels
+    self.tactile_max = [0] * self.num_taxels
+    self.tactile_labels = []
+    self.tactile_lights = []
+    self.tactile_desc = []
+    self.test_grid.addWidget(self.accel_mag_light, 0, 0)
+    self.test_grid.addWidget(self.accel_mag_label, 0, 1)
+    for i in xrange(0, self.num_taxels):
+      self.tactile_lights.append(QLabel())
+      self.tactile_lights[i].setStyleSheet("QWidget {background-color:red}")
+      self.test_grid.addWidget(self.tactile_lights[i], i+1, 0)
+      self.tactile_labels.append(QLabel("0"))
+      self.test_grid.addWidget(self.tactile_labels[i], i+1, 1)
+      self.tactile_desc.append(QLabel("Tactile %d" % i))
+      self.test_grid.addWidget(self.tactile_desc[i])
+    button_hbox = QHBoxLayout()
+    button_hbox.addWidget(self.bootloader_btn)
+    button_hbox.addWidget(self.application_btn)
+    button_hbox.addWidget(self.auto_btn)
+    button_hbox.addStretch(1)
+    self.vbox = QVBoxLayout()
+    self.vbox.addLayout(button_hbox)
+    self.vbox.addLayout(self.test_grid)
+    self.vbox.addStretch(1)
+    self.vbox.addWidget(self.terminal)
+    self.setLayout(self.vbox)
+  def onUpdateUI(self, accel_raw, tactile_raw, auto_exit_on_success):
+    accel_mag = 0
+    for i in xrange(0,3):
+      accel_mag += accel_raw[i]**2
+    accel_mag = math.sqrt(accel_mag)
+    tactile_range = [0] * len(tactile_raw)
+    for i in xrange(0, len(tactile_raw)):
+      t = tactile_raw[i]
+      if t > self.tactile_max[i]:
+        self.tactile_max[i] = t
+      if t < self.tactile_min[i]:
+        self.tactile_min[i] = t
+      tactile_range[i] = self.tactile_max[i] - self.tactile_min[i]
+    self.accel_mag_label.setText("%d" % accel_mag)
+    ready_to_exit = self.exit_on_success
+    if (accel_mag > 800 and accel_mag < 1200):
+      self.accel_mag_light.setStyleSheet("QWidget {background-color:green}")
+    else:
+      ready_to_exit = False
+      self.accel_mag_light.setStyleSheet("QWidget {background-color:red}")
+    for i in xrange(0, len(tactile_raw)):
+      self.tactile_desc[i].setText("Tactile %02d: %d" % (i, tactile_raw[i]))
+      self.tactile_labels[i].setText("%d" % tactile_range[i])
+      if tactile_range[i] > 3000:
+        self.tactile_lights[i].setStyleSheet("QWidget {background-color:green}")
+      else:
+        ready_to_exit = False
+        self.tactile_lights[i].setStyleSheet("QWidget {background-color:red}")
+    if ready_to_exit:
+      if self.driver_process:
+        print "stopping driver process"
+        os.system("rosnode kill sandia_hand_loose_finger_node")
+        print "sleeping..."
+        os.system("sleep 0.25")
+        print "quitting."
+        self.driver_process = None
+        QApplication.quit()
+  def bootloaderClicked(self):
+    cmd = "cd %s && bin/loose_finger_cli /dev/ttyUSB0 pb on && sleep 1 && cd %s/../../firmware/build && make %s-bl-gpnvm && make %s-bl-program && echo \"\ntasks complete\"" % (self.shd, self.shd, self.board_name, self.board_name)
+    self.process = QProcess(self)
+    self.process.finished.connect(self.bootloaderOnFinished)
+    self.process.setProcessChannelMode(QProcess.MergedChannels)
+    self.process.readyReadStandardOutput.connect(self.terminalReadOutput)
+    self.process.start("/bin/bash", ["-c", cmd])
+  def bootloaderOnFinished(self, exitCode, exitStatus):
+    if exitCode != 0:
+      QMessageBox.about(self, "bad!", "error detected. see terminal output.")
+  def autoOnFinished(self, exitCode, exitStatus):
+    if exitCode != 0:
+      QMessageBox.about(self, "bad!", "error detected. see terminal output.")
+    if self.board_name == "f3":
+      cmd = "cd %s && bin/sandia_hand_loose_finger_node _use_proximal_phalange:=false" % self.shd
+    elif self.board_name == "f2":
+      cmd = "cd %s && bin/sandia_hand_loose_finger_node _use_distal_phalange:=false" % self.shd
+    else:
+      QMessageBox.about(self, "bad!", "auto button not ready for this board")
+      return
+    self.driver_process = QProcess(self)
+    self.driver_process.setProcessChannelMode(QProcess.MergedChannels)
+    self.driver_process.start("/bin/bash", ["-c", cmd])
+  def autoClicked(self):
+    bn = self.board_name
+    shd = self.shd
+    cmd = "cd %s && bin/loose_finger_cli /dev/ttyUSB0 pb on && sleep 1 && cd %s/../../firmware/build && make %s-bl-gpnvm && make %s-bl-program && cd %s && bin/loose_finger_cli /dev/ttyUSB0 dburn ../../firmware/build/%s/std/%s-std.bin && echo \"\ntasks complete\"" % (shd, shd, bn, bn, shd, bn, bn)
+    self.process = QProcess(self)
+    self.process.finished.connect(self.autoOnFinished)
+    self.process.setProcessChannelMode(QProcess.MergedChannels)
+    self.process.readyReadStandardOutput.connect(self.terminalReadOutput)
+    self.process.start("/bin/bash", ["-c", cmd])
+  def applicationClicked(self):
+    bn = self.board_name
+    cmd = "cd %s && bin/loose_finger_cli /dev/ttyUSB0 dburn ../../firmware/build/%s/std/%s-std.bin && echo \"\ntasks complete\"" % (self.shd, bn, bn)
+    self.process = QProcess(self)
+    self.process.finished.connect(self.bootloaderOnFinished)
+    self.process.setProcessChannelMode(QProcess.MergedChannels)
+    self.process.readyReadStandardOutput.connect(self.terminalReadOutput)
+    self.process.start("/bin/bash", ["-c", cmd])
+  def terminalReadOutput(self):
+    #sys.stdout.write(self.process.readAllStandardOutput())
+    self.terminal.append(QString(self.process.readAllStandardOutput()))
+
 class MaintenanceWindow(QWidget):
   def __init__(self):
     super(MaintenanceWindow, self).__init__()
-    self.f3_bootloader_btn = QPushButton("Install Bootloader via JTAG")
-    self.f3_bootloader_btn.clicked.connect(self.f3BootloaderClicked)
-    self.f3_application_btn = QPushButton("Install Application over USB/serial")
-    self.f3_application_btn.clicked.connect(self.f3ApplicationClicked)
-    self.f3_go_btn = QPushButton("Awesome")
-    self.f3_go_btn.clicked.connect(self.f3GoClicked)
-    self.f3_terminal = QTextEdit()
-    self.f3_terminal.setStyleSheet("QTextEdit { background-color: black; color: white;}")
-    self.driver_process = None
     self.tab_widget = QTabWidget()
-    f3_firmware_tab = QWidget()
-    f3_test_tab = QWidget()
-    self.tab_widget.addTab(f3_firmware_tab, "F3 Firmware")
-    self.tab_widget.addTab(f3_test_tab, "F3 Test")
-    f3_firmware_vbox = QVBoxLayout(f3_firmware_tab)
-    f3_firmware_vbox.addWidget(self.f3_bootloader_btn)
-    f3_firmware_vbox.addWidget(self.f3_application_btn)
-    f3_firmware_vbox.addWidget(self.f3_go_btn)
-    f3_firmware_vbox.addWidget(self.f3_terminal)
-    f3_firmware_vbox.addStretch(1)
-    f3_test_grid = QGridLayout(f3_test_tab)
-    f3_test_grid.setSpacing(5)
-    f3_test_grid.setColumnMinimumWidth(0, 15)
-    f3_test_grid.setColumnStretch(2, 1)
-    f3_test_grid.addWidget(QLabel("Accelerometer magnitude"), 0, 2)
-    self.f3_accel_mag_light = QLabel() #"FAIL")
-    self.f3_accel_mag_light.setStyleSheet("QWidget {background-color:red}")
-    self.f3_accel_mag_label = QLabel("0")
-    self.f3_tactile_min = [65535] * 12
-    self.f3_tactile_max = [0] * 12
-    self.f3_tactile_labels = []
-    self.f3_tactile_lights = []
-    self.f3_tactile_desc = []
-    f3_test_grid.addWidget(self.f3_accel_mag_light, 0, 0)
-    f3_test_grid.addWidget(self.f3_accel_mag_label, 0, 1)
-    for i in xrange(0,12):
-      self.f3_tactile_lights.append(QLabel())
-      self.f3_tactile_lights[i].setStyleSheet("QWidget {background-color:red}")
-      f3_test_grid.addWidget(self.f3_tactile_lights[i], i+1, 0)
-      self.f3_tactile_labels.append(QLabel("0"))
-      f3_test_grid.addWidget(self.f3_tactile_labels[i], i+1, 1)
-      self.f3_tactile_desc.append(QLabel("Tactile %d" % i))
-      test_grid.addWidget(self.tactile_desc[i])
+    # todo: parse which tab is auto exit worthy
+    self.f3_tab = TactileBoardTab("f3", 12, False) 
+    self.f2_tab = TactileBoardTab("f2",  6, False)
+    self.tab_widget.addTab(self.f3_tab, "F3")
+    self.tab_widget.addTab(self.f2_tab, "F2")
 
     vbox = QVBoxLayout()
     vbox.addWidget(self.tab_widget)
-    self.setGeometry(850, 100, 600, 300)
+    self.setGeometry(850, 100, 600, 600)
     self.setLayout(vbox)
     self.setWindowTitle('Sandia Hand Maintenance')
-    self.shd = roslib.packages.get_pkg_dir('sandia_hand_driver')
     #cp = QDesktopWidget().availableGeometry().center()
     #qr = self.frameGeometry()
     #qr.moveCenter(cp)
@@ -67,102 +160,23 @@ class MaintenanceWindow(QWidget):
     self.show()
     self.finger_sub = rospy.Subscriber('raw_state', RawFingerState, 
                                        self.finger_status_cb)
-    self.connect(self, SIGNAL('updateUI'), self.onUpdateUI)
+    self.connect(self, SIGNAL('updateF3'), self.f3_tab.onUpdateUI)
+    self.connect(self, SIGNAL('updateF2'), self.f2_tab.onUpdateUI)
   def finger_status_cb(self, msg):
-    #print "finger status"
-    f3_accel_mag_squared = 0
-    for i in xrange(0,3):
-      f3_accel_mag_squared += msg.dp_accel[i]**2
-    f3_accel_mag = math.sqrt(f3_accel_mag_squared)
-    f3_tactile_raw = [0]*12
-    f3_tactile_range = [0]*12
-    for i in xrange(0,12):
-      t = msg.dp_tactile[i]
-      f3_tactile_raw[i] = t
-      if t > self.f3_tactile_max[i]:
-        self.f3_tactile_max[i] = t
-      if t < self.f3_tactile_min[i]:
-        self.f3_tactile_min[i] = t
-      f3_tactile_range[i] = self.f3_tactile_max[i] - self.f3_tactile_min[i]
-    self.emit(SIGNAL('updateUI'), f3_accel_mag, f3_tactile_raw, f3_tactile_range)
-  def onUpdateUI(self, f3_accel_mag, f3_tactile_raw, f3_tactile_range):
-    #print "onUpdateUI %d" % f3_accel_mag
-    self.f3_accel_mag_label.setText("%d" % f3_accel_mag)
-    #all_ok = True
-    all_ok = False
-    if (f3_accel_mag > 800 and f3_accel_mag < 1200):
-      self.f3_accel_mag_light.setStyleSheet("QWidget {background-color:green}")
-      #self.accel_mag_light.setText("OK")
-    else:
-      all_ok = False
-      self.f3_accel_mag_light.setStyleSheet("QWidget {background-color:red}")
-      #self.accel_mag_light.setText("FAIL")
-    for i in xrange(0,12):
-      self.f3_tactile_desc[i].setText("Tactile %02d: %d" % (i, f3_tactile_raw[i]))
-      self.f3_tactile_labels[i].setText("%d" % f3_tactile_range[i])
-      if f3_tactile_range[i] > 3000:
-        self.f3_tactile_lights[i].setStyleSheet("QWidget {background-color:green}")
-        #self.tactile_lights[i].setText("OK")
-      else:
-        all_ok = False
-        self.f3_tactile_lights[i].setStyleSheet("QWidget {background-color:red}")
-        #self.tactile_lights[i].setText("FAIL")
-    if all_ok:
-      if self.driver_process:
-        print "stopping driver process"
-        #self.driver_process.terminate() 
-        #print "sleeping..."
-        #cmd = "cd %s && bin/loose_finger_cli /dev/ttyUSB0 pb on && sleep 1 && cd %s/../../firmware/build && make f3-bl-gpnvm && make f3-bl-program && echo \"\ntasks complete\"" % (self.shd, self.shd)
-        #self.process = QProcess(self)
-        #self.process.finished.connect(self.bootloaderOnFinished)
-        #self.process.setProcessChannelMode(QProcess.MergedChannels)
-        #self.process.readyReadStandardOutput.connect(self.terminalReadOutput)
-        #self.process.start("/bin/bash", ["-c", cmd])
-        os.system("rosnode kill sandia_hand_loose_finger_node")
-        print "sleeping..."
-        os.system("sleep 0.25")
-        print "quitting."
-        self.driver_process = None
-        QApplication.quit()
-  def f3BootloaderClicked(self):
-    cmd = "cd %s && bin/loose_finger_cli /dev/ttyUSB0 pb on && sleep 1 && cd %s/../../firmware/build && make f3-bl-gpnvm && make f3-bl-program && echo \"\ntasks complete\"" % (self.shd, self.shd)
-    self.process = QProcess(self)
-    self.process.finished.connect(self.f3BootloaderOnFinished)
-    self.process.setProcessChannelMode(QProcess.MergedChannels)
-    self.process.readyReadStandardOutput.connect(self.f3TerminalReadOutput)
-    self.process.start("/bin/bash", ["-c", cmd])
-  def f3BootloaderOnFinished(self, exitCode, exitStatus):
-    #print "load bootloader finished, exitCode %d" % exitCode
-    if exitCode != 0:
-      QMessageBox.about(self, "error!", "error detected. see terminal output.")
-  def f3GoOnFinished(self, exitCode, exitStatus):
-    if exitCode != 0:
-      QMessageBox.about(self, "error!", "error detected. see terminal output.")
-    else:
-      self.tab_widget.setCurrentIndex(1)
-    self.driver_process = QProcess(self)
-    cmd = "cd %s && bin/sandia_hand_loose_finger_node _use_proximal_phalange:=false" % self.shd
-    self.driver_process.setProcessChannelMode(QProcess.MergedChannels)
-    self.driver_process.start("/bin/bash", ["-c", cmd])
-  def f3GoClicked(self):
-    cmd = "cd %s && bin/loose_finger_cli /dev/ttyUSB0 pb on && sleep 1 && cd %s/../../firmware/build && make f3-bl-gpnvm && make f3-bl-program && cd %s && bin/loose_finger_cli /dev/ttyUSB0 dburn ../../firmware/build/f3/std/f3-std.bin && echo \"\ntasks complete\"" % (self.shd, self.shd, self.shd)
-    self.process = QProcess(self)
-    self.process.finished.connect(self.f3GoOnFinished)
-    self.process.setProcessChannelMode(QProcess.MergedChannels)
-    self.process.readyReadStandardOutput.connect(self.f3TerminalReadOutput)
-    self.process.start("/bin/bash", ["-c", cmd])
-  def f3ApplicationClicked(self):
-    cmd = "cd %s && bin/loose_finger_cli /dev/ttyUSB0 dburn ../../firmware/build/f3/std/f3-std.bin && echo \"\ntasks complete\"" % (self.shd)
-    self.process = QProcess(self)
-    self.process.finished.connect(self.f3BootloaderOnFinished)
-    self.process.setProcessChannelMode(QProcess.MergedChannels)
-    self.process.readyReadStandardOutput.connect(self.f3TerminalReadOutput)
-    self.process.start("/bin/bash", ["-c", cmd])
-  def f3TerminalReadOutput(self):
-    #sys.stdout.write(self.process.readAllStandardOutput())
-    self.f3_terminal.append(QString(self.process.readAllStandardOutput()))
-  def bootloaderOnReadErrors(self):
-    print "readerrors"
+    # copy everything out of the ROS thread and into UI threads
+    f3_accel_raw = [0] * 3
+    f2_accel_raw = [0] * 3
+    f3_tactile_raw = [0] * 12
+    f2_tactile_raw = [0] *  6
+    for i in xrange(0, 3):
+      f3_accel_raw[i] = msg.dp_accel[i]
+      f2_accel_raw[i] = msg.pp_accel[i]
+    for i in xrange(0, 12):
+      f3_tactile_raw[i] = msg.dp_tactile[i]
+    for i in xrange(0,  6):
+      f2_tactile_raw[i] = msg.pp_tactile[i]
+    self.emit(SIGNAL('updateF3'), f3_accel_raw, f3_tactile_raw)
+    self.emit(SIGNAL('updateF2'), f2_accel_raw, f2_tactile_raw)
 
 if __name__ == '__main__':
   rospy.init_node('sandia_hand_maintenance')
